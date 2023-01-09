746dc7614b0ab710282ab6c3f546addd18b17b24
bud-caolibin <caolibin@budcreate.io>
2023-01-09 12:06:44 +0800

    feat:增加memory模块

---
+++ memory/player.go
@@ -0,0 +1,32 @@
+package memory
+
+import "sync"
+
+var player2roomMap sync.Map
+
+func AddRoomToPlayerMap(roomId string, playerId string) {
+	if val, ok := player2roomMap.Load(playerId); ok {
+		if id, ok := val.(string); ok {
+			if id != roomId {
+				player2roomMap.Store(playerId, roomId)
+			}
+		}
+	} else {
+		player2roomMap.Store(playerId, roomId)
+	}
+}
+
+func RemoveRoomFromPlayerMap(playerId string) {
+	if _, ok := player2roomMap.Load(playerId); ok {
+		player2roomMap.Delete(playerId)
+	}
+}
+
+func GetRoomIdByPlayerId(playerId string) string {
+	if val, ok := player2roomMap.Load(playerId); ok {
+		if roomId, ok := val.(string); ok {
+			return roomId
+		}
+	}
+	return ""
+}
